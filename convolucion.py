bf30bd72e06537cdb901d316f1716c77647eee17
G0ssamer <charly.cerv.17@gmail.com>
2021-03-19 08:34:30 -0600

    nuevos archivos

--- convolucion.py
+++ convolucion.py
@@ -1,9 +1,10 @@
 import numpy as np
+import cv2
 
 def convolucion(Ioriginal,Kernel):
     fr = len(Ioriginal)-(len(Kernel)-1)
     cr = len(Ioriginal[0])-(len(Kernel[0])-1)
-    Resultado = np.zeros((fr,cr))
+    Resultado = np.zeros((fr,cr),np.uint8)
 
     #For para recorrer filas 
     for i in range(len(Resultado)):
@@ -16,7 +17,10 @@ def convolucion(Ioriginal,Kernel):
             for m in range(len(Kernel)):
                 for n in range(len(Kernel[0])):
                     suma += Kernel[m][n] * Ioriginal[m + i][n + j]
-            Resultado[i][j] = suma
+            if suma <= 255:
+                Resultado[i][j] = round(suma)
+            else:
+                Resultado[i][j] = 255
     return Resultado
 
 #Imagenes 
@@ -27,7 +31,13 @@ I = [[2,0,1,1,2],[3,0,0,0,2],[1,1,1,1,1],[3,1,1,1,2],[1,1,1,1,1]]
 In = np.array(I)
 Kn = np.array(K)
 
+IRGB = cv2.imread('002.jpg')
+IGS = cv2.cvtColor(IRGB,cv2.COLOR_BGR2GRAY)
+print(IGS.shape)
+
 #Funcion de devolucion 
-R = convolucion(In,Kn)
+R = convolucion(IGS,Kn)
 print(R)
+print(R.shape)
+cv2.imwrite('002C.jpg',R)
 
